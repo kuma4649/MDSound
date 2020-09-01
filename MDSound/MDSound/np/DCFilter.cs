d58b1d38630054f0c35ae25cd3513ce8a61c1e3d
kumatan <kumatan@kumymelody.jp>
2020-09-01 23:36:11 +0900

    TAG054 2020/09/01   ・NSF向けHPF,LPFを追加(NSFPlayから移植)

---
+++ MDSound/MDSound/np/DCFilter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDSound.np
+{
+    //
+    // filter.h から抜粋
+    //
+    //
+
+
+
+    //  dcf.SetParam(270,(*config)["HPF"]);    //DCFilter
+    //  lpf.SetParam(4700.0,(*config)["LPF"]); //Filter
+
+
+    public class DCFilter//HPF
+    {
+        private double R, C;
+        private double a;
+        private double[] _in = new double[2], _out = new double[2];
+        private double rate;
+
+        public DCFilter()
+        {
+            R = C = 0.0;
+            Reset();
+        }
+
+        ~DCFilter()
+        {
+        }
+
+        public void UpdateFactor()
+        {
+            if (C == 0.0 || R == 0.0)
+            {
+                a = 2.0; // disable
+            }
+            else
+            {
+                a = (R * C) / ((R * C) + (1.0 / rate));
+            }
+        }
+
+        public double GetFactor() { return a; }
+
+        public void SetRate(double r)
+        {
+            // カットオフ周波数 : 2pi*R*C
+            rate = r;
+            UpdateFactor();
+        }
+
+        public void SetParam(double r, int c) // c = 0-256, 256 = off, 0 = max
+        {
+            R = r;
+            //C = c;
+
+            if (c > 255)
+                C = 0.0; // disable
+            else
+                C = 2.0e-4 * (1.0 - Math.Pow(1.0 - ((double)(c + 1) / 256.0), 0.05));
+
+            // the goal of this curve is to have a wide range of practical use,
+            // though it may look a little complicated. points of interest:
+            //   HPF = 163 ~ my NES
+            //   HPF = 228 ~ my Famicom
+            //   low values vary widely and have an audible effect
+            //   high values vary slowly and have a visible effect on DC offset
+
+            UpdateFactor();
+        }
+
+        // 非virtualなRender
+        public uint FastRender(int[] b)//[2])
+        {
+            if (a < 1.0)
+            {
+                _out[0] = a * (_out[0] + b[0] - _in[0]);
+                _in[0] = b[0];
+                b[0] = (int)_out[0];
+
+                _out[1] = a * (_out[1] + b[1] - _in[1]);
+                _in[1] = b[1];
+                b[1] = (int)_out[1];
+            }
+            return 2;
+        }
+
+        public uint Render(int[] b)//[2])
+        {
+            return FastRender(b);
+        }
+
+        public void Tick(uint clocks)
+        {
+        }
+
+        public void Reset()
+        {
+            _in[0] = _in[1] = 0;
+            _out[0] = _out[1] = 0;
+        }
+
+        public void SetLevel(int[] b)//[2])
+        {
+            _in[0] = b[0];
+            _in[1] = b[1];
+        }
+    }
+
+
+
+}
---
+++ MDSound/MDSound/np/Filter.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDSound.np
+{
+    //
+    // filter.h から抜粋
+    //
+    //
+
+
+
+    public class Filter
+    {
+        protected IRenderable target;
+        protected int type;
+        protected int[] _out = new int[2];
+        protected double a;
+        protected double rate, R, C;
+        protected bool disable;
+        protected int GETA_BITS;
+
+        public Filter()
+        {
+            GETA_BITS = (20);
+            target = null;
+            rate = 48000;// DEFAULT_RATE;
+            R = 4700;
+            C = 10.0E-9;
+            disable = false;
+            _out[0] = _out[1] = 0;
+        }
+
+        ~Filter()
+        {
+        }
+
+        public void Attach(IRenderable t)
+        {
+            target = t;
+        }
+
+        public uint FastRender(int[] b)//[2])
+        {
+            if (target != null) target.Render(b);
+            if (a < 1.0)
+            {
+                _out[0] += (int)(a * (b[0] - _out[0]));
+                _out[1] += (int)(a * (b[1] - _out[1]));
+                b[0] = _out[0];
+                b[1] = _out[1];
+            }
+            return 2;
+        }
+
+        public void Tick(uint clocks)
+        {
+            if (target != null) target.Tick(clocks);
+        }
+
+        public uint Render(int[] b)//[2])
+        {
+            return FastRender(b);
+        }
+
+        public void SetParam(double r, int c) // c = 0-400, 0=off, 400=max
+        {
+            //C = 1.0E-10 * c;
+            R = r;
+
+            C = Math.Pow((double)(c) / 400.0, 2.0) * 1.0E-10 * 400.0;
+            // curved to try to provide useful range of settings
+            //   LPF = 112 ~ my NES
+
+            UpdateFactor();
+        }
+
+        public void SetClock(double clock)
+        {
+            Reset();
+        }
+
+        public void SetRate(double r)
+        {
+            rate = r;
+            UpdateFactor();
+        }
+
+        public void UpdateFactor()
+        {
+            if (R != 0.0 && C != 0.0 && rate != 0.0)
+                a = (1.0 / rate) / ((R * C) + (1.0 / rate));
+            else
+                a = 2.0; // disabled
+        }
+
+        public double GetFactor() { return a; }
+
+        public void Reset()
+        {
+            UpdateFactor();
+            _out[0] = _out[1] = 0;
+        }
+
+    }
+
+
+
+}
